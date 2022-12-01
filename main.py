4055c9d57762e26608818c39e2fa14b36f66994f
Giovani Martins <gio@Giovanis-MacBook-Pro.local>
2022-12-01 09:45:34 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+#Giovani Martins
+#Problems 1-I cant make it print backwards when I put -1 as the last value in range it doesn't print anything
+# tried making appending the numbers in list a list and reversing them, which worked but it looked wierd and the use of lists wasn't in the prompt
+for number in range(1,200):
+    multiple = 3
+    if number%3 == 0:
+        print(number)
